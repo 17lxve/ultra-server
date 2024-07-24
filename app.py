9ccd7043fd7ca1d72293a5565d98823e48e2f20f
Timmy <kouadiobhegnino@gmail.com>
2024-07-24 10:59:28 +0000

    done_at_last

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, request
+from flask import Flask, logging, request, jsonify
 from flask_cors import CORS, cross_origin
 from werkzeug.utils import secure_filename
 import subprocess
 
 app = Flask(__name__)
-cors = CORS(app)
+CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+@cross_origin()
 @app.route("/")
-# @cross_origin
 def test():
   return "<h1> Live Test </h1><p>Server is active</p>"
 
+@cross_origin()
 @app.route('/submit/image', methods=['POST'])
-# @cross_origin
 def submit_image():
   image = request.files['image']
   image.save('./image.jpg')
@@ -21,7 +21,8 @@ def submit_image():
   cmd = ['python', 'scripts/image.py']
   output = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
   print(output)
-  return "{result:" + f"{output}" +"}"
+  response = jsonify({"result": output.__str__()})
+  return response
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5001, debug=True)
